0bc7ce2ba5d8c3b50f96ba1560f0a5aedffccc5e
John Berryman <jfberryman@gmail.com>
2012-11-26 09:46:19 -0500

    Added incomplete code to accept PayPal Instant Payment Notifications

--- spurapp/urls.py
+++ spurapp/urls.py
@@ -1,18 +1,12 @@
 from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
-from django.contrib import admin
-admin.autodiscover()
 
-urlpatterns = patterns('',
-    url(r'^spurapp/', include('spurapp.urls')),
+urlpatterns = patterns('spurapp.views',
+    url(r'^ipn$', 'ipn'),
     # Examples:
     # url(r'^$', 'spurproject.views.home', name='home'),
     # url(r'^spurproject/', include('spurproject.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
 )
