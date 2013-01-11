d384048211713fe9f47d25871d029c2708ad209a
Krystal <krystalxxkat@gmail.com>
2013-01-10 21:03:38 -0500

    url fix

--- spurapp/urls.py
+++ spurapp/urls.py
@@ -6,7 +6,6 @@ from spurapp.models import *
 urlpatterns = patterns('spurapp.views',
     url(r'^ipn$', 'ipn'),
     url(r'^$', 'index'),
-    url(r'^charity/(?P<charity_id>\d+)/$', 'charity_detail'),
     url(r'^charity/(?P<charity_id>\d+)/campaign/$', 'campaign'),
     url(r'^redirect/(?P<donation_id>\d+)', 'redirect'),
     url(r'^share/(?P<campaign_id>\d+)', 'share'),
